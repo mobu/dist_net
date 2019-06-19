5558f23f7793f88023044710b06db95c50b771d2
Mubasser Kamal <mkamal@netspi.com>
2019-06-19 13:12:56 -0500

    Update pointer.go

--- pointer.go
+++ pointer.go
@@ -46,19 +46,6 @@ func main() {
 
 }
 
-func checkSum(msg []byte) uint16 {
-    sum := 0
-
-    // assume even for now
-    for n := 1; n < len(msg)-1; n += 2 {
-        sum += int(msg[n])*256 + int(msg[n+1])
-    }
-    sum = (sum >> 16) + (sum & 0xffff)
-    sum += (sum >> 16)
-    var answer uint16 = uint16(^sum)
-    return answer
-}
-
 func wakeOnLan(ip string) {
 	//	 port to connect to
 	var port_num int
