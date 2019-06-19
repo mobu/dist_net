356c069e8b363a5b6fa20894883e68f7b1aed14a
Mubasser Kamal <mkamal@netspi.com>
2019-06-19 11:01:18 -0500

    port and ip string

--- pointer.go
+++ pointer.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"net"
 	"os"
-	//"strings"
+	"strings"
+	"strconv"
 )
 
 func main() {
@@ -29,13 +30,28 @@ func main() {
 		}
 		fmt.Println(char)
 	}
+	wakeOnLan("127.0.0.1:80")
+
 }
 
-func wakeOnLan(ip String,mac String){
-	addr, err := net.ResolveIPAddr("ip",net.ParseIP(ip))
+func wakeOnLan(ip string){
+	//	 port to connect to
+	var port_num int
+	//	split the ip string to check for any port number
+	ip_addr := strings.Split(ip,":")
+	//	if port is given, store it in port_num or
+	//	else assign port_num a default port value (UDP 9)
+	if len(ip_addr[1]) > 0{
+		port_num,_ = strconv.Atoi(ip_addr[1])
+	}else{
+		port_num = 9
+	}
+	//resolve the IP address
+	addr, err := net.ResolveIPAddr("ip",ip_addr[0])
 	if err != nil{
 		fmt.Println("Resolution error",err.Error())
 		os.Exit(1)
 	}
-
+	fmt.Println(addr.IP)
+	fmt.Println(port_num)
 }
